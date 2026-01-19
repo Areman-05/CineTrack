9ebaa6d2cc2dce7a374bfcafa9613acc0d5f3bb1
Mateo <mateo.acha@gracia.lasalle.cat>
2026-01-19 17:36:55 +0100

    Clean: Remove error message from UserPreference.swift

--- Models/UserPreference.swift
+++ Models/UserPreference.swift
@@ -10,8 +10,3 @@ struct UserPreference: Codable {
         self.personalNote = personalNote
     }
 }
-
-
-Showing Recent Messages
-Build input files cannot be found: '/Users/alumne/Documents/CineTrack6/CineTrack/Views/DetailView.swift', '/Users/alumne/Documents/CineTrack6/CineTrack/Views/ExplorarView.swift', '/Users/alumne/Documents/CineTrack6/CineTrack/Services/TMDBService.swift', '/Users/alumne/Documents/CineTrack6/CineTrack/ViewModels/MovieViewModel.swift', '/Users/alumne/Documents/CineTrack6/CineTrack/Views/BuscarView.swift', '/Users/alumne/Documents/CineTrack6/CineTrack/ContentView.swift', '/Users/alumne/Documents/CineTrack6/CineTrack/CineTrackApp.swift', '/Users/alumne/Documents/CineTrack6/CineTrack/Views/FavoritosView.swift', '/Users/alumne/Documents/CineTrack6/CineTrack/Models/UserPreference.swift', '/Users/alumne/Documents/CineTrack6/CineTrack/Models/Movie.swift'
-
